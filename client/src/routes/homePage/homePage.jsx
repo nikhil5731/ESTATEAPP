64ab8fc7158e2c15f244fd6a77dc6c2f654703f3
Pawan Kumar <ts5151822@gmail.com>
2024-06-16 15:58:11 +0530

    test the functionalities of the site

--- client/src/routes/homePage/homePage.jsx
+++ client/src/routes/homePage/homePage.jsx
@@ -13,14 +13,15 @@ function HomePage() {
         <div className="wrapper">
           <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
-            explicabo suscipit cum eius, iure est nulla animi consequatur
-            facilis id pariatur fugit quos laudantium temporibus dolor ea
-            repellat provident impedit!
+          Welcome to our Real Estate Marketplace!
+           Discover your dream property with ease—explore listings,
+            chat directly with owners,
+             and find the perfect location using our interactive maps.  <br/>
+             <div className="main">Start your journey towards a new home today!</div> 
           </p>
           <SearchBar />
           <div className="boxes">
-            <div className="box">
+            <div className="box">   
               <h1>16+</h1>
               <h2>Years of Experience</h2>
             </div>
