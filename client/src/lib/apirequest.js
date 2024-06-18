c1741d4394f4368feee63378abe308595a8264c1
Pawan Kumar <ts5151822@gmail.com>
2024-06-18 16:11:18 +0530

    cors

--- client/src/lib/apirequest.js
+++ client/src/lib/apirequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8800/api",
+  baseURL: "https://estate-app-swn3.onrender.com/api",
   withCredentials: true,
 });
 
