71d289b8e73f88bc8230d02877be2bfe58f892af
Nikhil Kumar <nikhilgraphicd@gmail.com>
2024-06-18 23:00:19 +0530

    cookie2

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -67,7 +67,6 @@ export const login = async (req, res) => {
 
     res
       .cookie("token", token, {
-        domain: ".onrender.com",
         path: "/",
         secure: true,
         httpOnly: true,
