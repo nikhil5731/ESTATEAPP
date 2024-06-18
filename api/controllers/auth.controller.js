e4cc40aa260665d671591667e8739a995e96dc48
Nikhil Kumar <nikhilgraphicd@gmail.com>
2024-06-18 21:42:55 +0530

    ehhehe

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -71,7 +71,7 @@ export const login = async (req, res) => {
         secure: true, // Ensures the cookie is only sent over HTTPS
         sameSite: "none", // Allows cross-site requests
         maxAge: age, // Expiration time in milliseconds
-        domain: ".vercel.app", // Set the domain for the cookie
+        domain: ".onrender.com", // Set the domain for the cookie
         path: "/", // Set the path for the cookie
       })
       .status(200)
