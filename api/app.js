157058f43abaec21dc8be028e2384dfc26c61c08
Pawan Kumar <ts5151822@gmail.com>
2024-06-18 16:22:41 +0530

    test1 cors

--- api/app.js
+++ api/app.js
@@ -11,6 +11,14 @@ import messageRoute from "./routes/message.route.js";
 const app = express();
 
 app.use(cors({ origin: "*", credentials: true }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 app.use(express.json());
 app.use(cookieParser());
 
